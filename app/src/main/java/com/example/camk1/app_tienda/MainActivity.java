981ae9817886bf7ba9a66e6602871c23b1d9843b
PabloTPZ <perez.zpt@gmail.com>
2016-11-26 23:41:19 -0400

    tienda

---
+++ app/src/main/java/com/example/camk1/app_tienda/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.camk1.app_tienda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
